80b8fe66e1ae34b729c8294e36c47369f4422000
Mark Torrance <mtorrance@rocketfuel.com>
2014-09-01 22:56:58 -0700

    Updated links for cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,10 +24,10 @@ set :deploy_to, '/home/mark/htdocs/tickets'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
